cd74bd3ce4f4047da46dc95888ff3e31cc8d0ab1
IVSHAH <shashovivan2@mail.ru>
2025-09-29 12:47:08 +0300

    fix: update docker and change host name

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule, AuthModule],
 })
 export class AppModule {}
